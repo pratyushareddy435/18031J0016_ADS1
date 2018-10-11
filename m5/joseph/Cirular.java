3f6a8ee477aace76f580914c9bbe64aae3c1b454
K.pratyusha reddy <pratyusha96sweety@gmail.com>
2018-10-11 12:50:04 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

---
+++ m5/joseph/Cirular.java
@@ -0,0 +1,33 @@
+package module5;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Cirular {
+
+	public static void main(String[] args) 
+	{
+		LinkedList<Integer> ll=new LinkedList<>();
+		Scanner sc=new Scanner(System.in);
+		int ni=sc.nextInt();
+		for(int i=0;i<ni;i++)
+		{
+			int n=sc.nextInt();
+			int m=sc.nextInt();
+			for(int j=0;j<n;j++)
+			{
+				ll.add(j);
+			}
+			int temp=0;
+			while(ll.size()!=0)
+			{
+				temp=(temp+m-1)%ll.size();
+				System.out.println(ll.get(temp));
+				
+				
+			}
+		}
+
+	}
+
+}
